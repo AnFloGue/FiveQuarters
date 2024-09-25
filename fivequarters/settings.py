2268a33c71de0c18199eecf1f74f84edd336e6a5
AnFlGue <mokshaflores@icloud.com>
2024-09-25 22:06:54 +0200

    Add WhiteNoise for static file handling
    Included WhiteNoise middleware to improve static file serving. Configured STATIC_ROOT to ensure proper collection of static files during deployment.

--- fivequarters/settings.py
+++ fivequarters/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -107,6 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
