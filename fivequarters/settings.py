917e2aa35367825d1d170a0a438799f13c6c430b
AnFlGue <mokshaflores@icloud.com>
2024-09-25 21:38:37 +0200

    Switch to PostgreSQL database configuration
    Updated the database settings in `fivequarters/settings.py` to switch from SQLite to PostgreSQL. Added environment variables for database name, user, password, host, and port to enhance security and flexibility in different environments. Removed trailing whitespace in the installed apps section for better code hygiene.

--- fivequarters/settings.py
+++ fivequarters/settings.py
@@ -26,9 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'rest_framework',
-    
+
     'accounts',
     'frontshop',
     'inventory',
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'fivequarters.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
