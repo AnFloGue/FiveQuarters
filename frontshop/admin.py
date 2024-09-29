e0084d6ae89f67e44b5d37dbf56945d2632cc73e
AnFlGue <mokshaflores@icloud.com>
2024-09-29 14:58:04 +0200

    Enhance Order model and templates
    Added DeliveryCompany model and new fields to Order for detailed tracking. Updated admin configurations and improved clarity in order detail and list templates.

--- frontshop/admin.py
+++ frontshop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, DeliveryCompany
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'status', 'total_price', 'created_at', 'updated_at')
+    list_display = ['id', 'user', 'status', 'total_price', 'posted_date', 'received_date', 'created_at']
     list_display_links = ('id', 'user')
     list_editable = ('status',)
     search_fields = ('user__username', 'status')
@@ -16,3 +17,9 @@ class OrderItemAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'order')
     search_fields = ('order__id', 'product__name')
     list_filter = ('order__status',)
+    
+@admin.register(DeliveryCompany)
+class DeliveryCompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
--- frontshop/models.py
+++ frontshop/models.py
@@ -2,18 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from inventory.models import Product
 
+class DeliveryCompany(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=50, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('canceled', 'Canceled')])
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(blank=True, null=True)
+    tracking_number = models.CharField(max_length=100, blank=True, null=True)
+    delivery_company = models.ForeignKey(DeliveryCompany, on_delete=models.SET_NULL, null=True, blank=True)
+    posted_date = models.DateTimeField(blank=True, null=True)
+    received_date = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
 
     def get_order_items(self):
-        return "\n".join([f"{item.quantity} of {item.product.name}" for item in self.items.all()])
+        items_list = []
+        for item in self.items.all():
+            items_list.append(f"{item.quantity} of {item.product.name}")
+        return "\n".join(items_list)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
